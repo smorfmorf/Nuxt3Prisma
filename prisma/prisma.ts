3badc039cd9c305c18556d0d9d688b778dd35679
M.P. Yuzik <m.p.yuzik@example.com>
2026-01-21 16:51:25 +1000

    ''

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,16 +1,15 @@
-import 'dotenv/config'
+import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "~/generated/prisma";
-
+import { PrismaClient } from "~/generated/prisma/client";
 
 const adapter = new PrismaBetterSqlite3({
   // можно взять из .env, как в доках
-  url: process.env.DATABASE_URL ?? 'file:./dev.db',
-})
+  url: process.env.DATABASE_URL ?? "file:./dev.db",
+});
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
-}
+  return new PrismaClient({ adapter });
+};
 
 // const prismaClientSingleton = () => {
 //   return new PrismaClient()
@@ -20,8 +19,8 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
--- server/api/users.post.ts
+++ server/api/users.post.ts
@@ -1,18 +1,17 @@
 // server/api/users.post.ts
-import prisma from "~/lib/prisma"
-import os from 'os'
-
+import prisma from "~/prisma/prisma";
+import os from "os";
 
 export default defineEventHandler(async (event) => {
   console.log(os.platform());
-  const body = await readBody(event)
+  const body = await readBody(event);
   const user = await prisma.user.create({
     data: {
       email: body.email,
       name: body.name,
       password: body.password,
     },
-  })
+  });
 
-  return user
-})
+  return user;
+});
